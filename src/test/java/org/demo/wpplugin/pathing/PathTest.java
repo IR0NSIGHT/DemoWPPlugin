60234f6618b76afb734be1b9aaa7297f5d2a9993
Ir0nsight <irnsght@googlemail.com>
2024-09-22 15:53:24 +0200

    more tests and some fixes for nth handle interpolation

--- src/test/java/org/demo/wpplugin/pathing/PathTest.java
+++ src/test/java/org/demo/wpplugin/pathing/PathTest.java
@@ -37,7 +37,7 @@ class PathTest {
         assertEquals(p, clone);
         assertNotSame(p, clone);
 
-        int idx = p.amountHandles()/2;
+        int idx = p.amountHandles() / 2;
         float[] someHandle = p.handleByIndex(idx).clone();
         p = p.setHandleByIdx(someHandle, idx);
         assertArrayEquals(p.handleByIndex(idx), someHandle);
@@ -187,7 +187,7 @@ class PathTest {
 
     @Test
     void continuousCurve() {
-        Path p = newFilledPath(10+2+1, RIVER_2D);
+        Path p = newFilledPath(10 + 2 + 1, RIVER_2D);
 
         float startV = 3;
         p = p.setHandleByIdx(setValue(p.handleByIndex(0), RIVER_RADIUS, startV), 0);
@@ -197,10 +197,10 @@ class PathTest {
         assertEquals(startV, getValue(p.handleByIndex(1), RIVER_RADIUS));
 
 
-        p = p.setHandleByIdx(setValue(p.handleByIndex(p.amountHandles()-1), RIVER_RADIUS, 17), p.amountHandles() - 1);
-        assertEquals(17, getValue(p.handleByIndex(p.amountHandles()-1), RIVER_RADIUS));
-        p = p.setHandleByIdx(setValue(p.handleByIndex(p.amountHandles()-2), RIVER_RADIUS, 17), p.amountHandles() - 2);
-        assertEquals(17, getValue(p.handleByIndex(p.amountHandles()-2), RIVER_RADIUS));
+        p = p.setHandleByIdx(setValue(p.handleByIndex(p.amountHandles() - 1), RIVER_RADIUS, 17), p.amountHandles() - 1);
+        assertEquals(17, getValue(p.handleByIndex(p.amountHandles() - 1), RIVER_RADIUS));
+        p = p.setHandleByIdx(setValue(p.handleByIndex(p.amountHandles() - 2), RIVER_RADIUS, 17), p.amountHandles() - 2);
+        assertEquals(17, getValue(p.handleByIndex(p.amountHandles() - 2), RIVER_RADIUS));
 
         for (int i = 2; i < p.amountHandles() - 2; i++) {
             p = p.setHandleByIdx(setValue(p.handleByIndex(i), RIVER_RADIUS, INHERIT_VALUE), i);
@@ -209,25 +209,112 @@ class PathTest {
 
         //set position for all points
         for (int i = 0; i < p.amountHandles(); i++) {
-            float[] newHandle = setPosition2D(p.handleByIndex(i), new float[]{i*10,37});
+            float[] newHandle = setPosition2D(p.handleByIndex(i), new float[]{i * 10, 37});
             p = p.setHandleByIdx(newHandle, i);
 
-            assertEquals(new Point(i*10,37), getPoint2D(p.handleByIndex(i)));
+            assertEquals(new Point(i * 10, 37), getPoint2D(p.handleByIndex(i)));
         }
 
         ArrayList<float[]> curve = p.continousCurve();
         ArrayList<Point> curveP = PointUtils.point2DfromNVectorArr(curve);
 
         int curvePointIdx = 0;
-        for (int curveX = getPoint2D(p.handleByIndex(1)).x; curveX <= getPoint2D(p.handleByIndex(p.amountHandles()-2)).x; curveX++) {
+        for (int curveX = getPoint2D(p.handleByIndex(1)).x; curveX <= getPoint2D(p.handleByIndex(p.amountHandles() - 2)).x; curveX++) {
             //iterate from first handle where curve starts to last handle where curve ends
-            assertEquals(curveX, curveP.get(curvePointIdx).x,"Point " + curveP.get(curvePointIdx) );
-            assertEquals(37, curveP.get(curvePointIdx).y,"Point " + curveP.get(curvePointIdx) );
+            assertEquals(curveX, curveP.get(curvePointIdx).x, "Point " + curveP.get(curvePointIdx));
+            assertEquals(37, curveP.get(curvePointIdx).y, "Point " + curveP.get(curvePointIdx));
 
             curvePointIdx++;
         }
         for (float[] point : curve) {
             System.out.println("#".repeat(Math.round(getValue(point, RIVER_RADIUS))));
+        }
+    }
+
+    @Test
+    void prepareEmptyHandlesForInterpolation() {
+        float[] incompleteHandles;
+        float[] completeHandles;
+        float[] expectedHandles;
+
+        //only one is set
+        incompleteHandles = new float[]{INHERIT_VALUE, INHERIT_VALUE, 7, INHERIT_VALUE, INHERIT_VALUE, INHERIT_VALUE};
+        completeHandles = Path.prepareEmptyHandlesForInterpolation(incompleteHandles, INHERIT_VALUE);
+        expectedHandles = new float[]{7, 7, 7, INHERIT_VALUE, 7, 7};
+        assertArrayEquals(expectedHandles, completeHandles);
+
+        //two are set
+        incompleteHandles = new float[]{5, INHERIT_VALUE, INHERIT_VALUE, INHERIT_VALUE, 7};
+        completeHandles = Path.prepareEmptyHandlesForInterpolation(incompleteHandles, INHERIT_VALUE);
+        expectedHandles = new float[]{5, 5, INHERIT_VALUE, 7, 7};
+        assertArrayEquals(expectedHandles, completeHandles);
+
+        //two are set but in wierd positions
+        incompleteHandles = new float[]{INHERIT_VALUE, 5, 7, INHERIT_VALUE, INHERIT_VALUE};
+        completeHandles = Path.prepareEmptyHandlesForInterpolation(incompleteHandles, INHERIT_VALUE);
+        expectedHandles = new float[]{5, 5, 7, 7, 7};
+        assertArrayEquals(expectedHandles, completeHandles);
+
+        //three or more are set but the first and last two are missing
+        incompleteHandles = new float[]{INHERIT_VALUE, INHERIT_VALUE, 5, INHERIT_VALUE, 6, INHERIT_VALUE, 7,
+                INHERIT_VALUE, 8,
+                INHERIT_VALUE, 9, INHERIT_VALUE, 10, INHERIT_VALUE, INHERIT_VALUE};
+        completeHandles = Path.prepareEmptyHandlesForInterpolation(incompleteHandles, INHERIT_VALUE);
+        expectedHandles = new float[]{5, 5, 5, INHERIT_VALUE, 6, INHERIT_VALUE, 7, INHERIT_VALUE, 8,
+                INHERIT_VALUE, 9, INHERIT_VALUE, 10, 10, 10};
+        assertArrayEquals(expectedHandles, completeHandles);
+
+
+        {        //not a single value is known
+            incompleteHandles = new float[]{INHERIT_VALUE, INHERIT_VALUE, INHERIT_VALUE, INHERIT_VALUE, INHERIT_VALUE,
+                    INHERIT_VALUE};
+            float[] finalIncompleteHandles = incompleteHandles;
+            assertThrows(IllegalArgumentException.class,
+                    () -> Path.prepareEmptyHandlesForInterpolation(finalIncompleteHandles, INHERIT_VALUE));
+        }
+
+        {    //handles are not long enough
+            incompleteHandles = new float[]{1, 2, 3};
+            float[] finalIncompleteHandles = incompleteHandles;
+            assertThrows(IllegalArgumentException.class,
+                    () -> Path.prepareEmptyHandlesForInterpolation(finalIncompleteHandles, INHERIT_VALUE));
+        }
+    }
+
+    @Test
+    void interpolateHandles() {
+        float[] incompleteHandles;
+        int[] curveByHandleIdx = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        float[] completeHandles;
+        float[] expectedHandles;
+
+        //simplest flat
+        incompleteHandles = new float[]{1, 1, INHERIT_VALUE, INHERIT_VALUE, INHERIT_VALUE, INHERIT_VALUE,
+                INHERIT_VALUE, INHERIT_VALUE, 1, 1};
+        assertTrue(Path.canBeInterpolated(incompleteHandles));
+        completeHandles = Path.interpolateHandles(incompleteHandles, curveByHandleIdx);
+        expectedHandles = new float[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        assertArrayEquals(expectedHandles, completeHandles, 0.01f);
+
+        //minimum length
+        incompleteHandles = new float[]{1, 4, 7, 19};
+        curveByHandleIdx = new int[]{0, 1, 2, 3};
+        assertTrue(Path.canBeInterpolated(incompleteHandles));
+        completeHandles = Path.interpolateHandles(incompleteHandles, curveByHandleIdx);
+        expectedHandles = new float[]{1, 4, 7, 19};
+        assertArrayEquals(expectedHandles, completeHandles, 0.01f);
+
+        //classic 4 point interpolation that satisfys convex hull property
+        incompleteHandles = new float[]{10, 10, INHERIT_VALUE, INHERIT_VALUE, INHERIT_VALUE, INHERIT_VALUE, 3, 3};
+        curveByHandleIdx = new int[]{0, 1, 2, 3, 4, 5, 6, 7};
+        assertTrue(Path.canBeInterpolated(incompleteHandles));
+        completeHandles = Path.interpolateHandles(incompleteHandles, curveByHandleIdx);
+        expectedHandles = new float[]{1, 4, 7, 19};
+
+        for (int i = 2; i < incompleteHandles.length - 2; i++) {
+            //satisfies convex hull property
+            assertTrue(completeHandles[i] < 10);
+            assertTrue(completeHandles[i] > 3);
         }
     }
 }
